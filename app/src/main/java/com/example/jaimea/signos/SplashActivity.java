d2816bbe8a352f4aa1d24cb632cd851f3d1f6143
jaimea <jajhonrod@gmail.com>
2016-09-12 19:56:46 -0500

    Adicion de navigation

--- app/src/main/java/com/example/jaimea/signos/SplashActivity.java
+++ app/src/main/java/com/example/jaimea/signos/SplashActivity.java
@@ -21,7 +21,7 @@ public class SplashActivity extends AppCompatActivity {
                 } catch (InterruptedException e){
                     e.printStackTrace();
                 } finally {
-                    Intent intent = new Intent(SplashActivity.this, MenuActivity.class);
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                     startActivity(intent);
 
                 }
